f450e19af12331d65968f1e83277652c60c27816
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2021-03-01 20:28:10 -0500

    Top 2

--- WAdb/Query/Query1.sql
+++ WAdb/Query/Query1.sql
@@ -1,1 +1,8 @@
-﻿SELECT top 1 SUM(Valor), Placa from TANQUEOS group by Placa Order by sum(valor) Desc
+﻿
+---- Top One -----
+SELECT top 1 SUM(Valor), Placa from TANQUEOS group by Placa Order by sum(valor) Desc
+
+------Top two -----
+
+SELECT TOP 1 total, Placa FROM (SELECT TOP 2 SUM(Valor) AS total, Placa FROM TANQUEOS GROUP BY Placa ORDER BY SUM(valor) DESC) AS X ORDER BY total ASC
+
